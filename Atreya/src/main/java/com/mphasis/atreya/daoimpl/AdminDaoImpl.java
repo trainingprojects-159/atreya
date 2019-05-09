767d0d96cc84de5ef2ee4e9c3ddb3bf8e1fb94fe
pallavi2212 <49521662+pallavi2212@users.noreply.github.com>
2019-05-09 09:39:00 +0530

    Add files via upload
    admin dao implementation addedd

---
+++ Atreya/src/main/java/com/mphasis/atreya/daoimpl/AdminDaoImpl.java
@@ -0,0 +1,32 @@
+package com.mphasis.atreya.daoimpl;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.mphasis.atreya.dao.AdminDao;
+import com.mphasis.atreya.entities.Admin;
+
+
+
+@Repository
+public class AdminDaoImpl implements AdminDao {
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	 public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public Admin login(String adminid, String pwd) 
+	{
+		Session session=sessionFactory.openSession();
+		Query query=session.createQuery("from Admin where adminid=:adminid and pwd=:pwd");
+		   query.setParameter("adminid", adminid);
+		   query.setParameter("pwd",pwd);
+		   Admin admin=(Admin) query.uniqueResult();
+		   return admin;
+	}
+
+}
