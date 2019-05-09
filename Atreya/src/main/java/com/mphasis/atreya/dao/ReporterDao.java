fc77375644d3a9047ee04d410e003e77b6ee29de
hemalatha995 <49521856+hemalatha995@users.noreply.github.com>
2019-05-09 09:41:39 +0530

    Update ReporterDao.java
    added methods

--- Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
+++ Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
@@ -1,5 +1,8 @@
 package com.mphasis.atreya.dao;
 
 public interface ReporterDao {
-
+public Reporter login(String repid,String pwd);
+	public void insertReporter(Reporter reporter);
+    public void deleteReporter(String repid);
+    public void updateReporter(Reporter reporter);
 }
