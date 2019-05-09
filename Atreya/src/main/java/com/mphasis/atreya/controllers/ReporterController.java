ea591233657fba953357c055fe781325b430a0ff
hemalatha995 <49521856+hemalatha995@users.noreply.github.com>
2019-05-09 14:57:28 +0530

    Add files via upload
    added controller for reporter

--- Atreya/src/main/java/com/mphasis/atreya/controllers/ReporterController.java
+++ Atreya/src/main/java/com/mphasis/atreya/controllers/ReporterController.java
@@ -1,14 +1,16 @@
 package com.mphasis.atreya.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.mphasis.atreya.entities.LeaveReport;
 import com.mphasis.atreya.entities.Reporter;
+import com.mphasis.atreya.service.LeaveReportService;
 import com.mphasis.atreya.service.ReporterService;
 
 
@@ -18,19 +20,25 @@ public class ReporterController {
     @Autowired
 	ReporterService reporterService;
     
-
-
-	public void setReporterService(ReporterService reporterService) {
+        public void setReporterService(ReporterService reporterService) {
 		this.reporterService = reporterService;
 	}
+	
 	@Autowired
     LeaveReportService leaveReportService;
 	
+	public void setLeaveReportService(LeaveReportService leaveReportService) {
+		this.leaveReportService = leaveReportService;
+	}
+
+	@RequestMapping(value="/login",method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public Reporter login(@RequestParam("repid")String repid,@RequestParam("pwd")String pwd) {
+		Reporter reporter=reporterService.login(repid,pwd);
+		return reporter;
+	}
 	
-	
-	@RequestMapping(value="/reporter/{lrid}",method=RequestMethod.PUT) 
-	public void updateReporter(@PathVariable("lrid")int lrid,@RequestBody LeaveReport lr) {
-		
-		this.leavereporterService.manageLeave(lr);
+	@RequestMapping(value="/leavereport/{lrid}",method=RequestMethod.PUT, produces=MediaType.APPLICATION_JSON_VALUE) 
+	public void updateleaveReport(@PathVariable("lrid")int lrid,@RequestBody LeaveReport lr) {
+		this.leaveReportService.manageLeave(lr);
 	}
 }
