5d6d0baa8056594666db2d33a681765c01dd3d4d
mahesh.bv <mahesh.bv@WKSBAN24TR17012.corp.mphasis.com>
2019-05-09 10:18:45 +0530

    dao added

--- Atreya/src/main/java/com/mphasis/atreya/dao/AppointmentDao.java
+++ Atreya/src/main/java/com/mphasis/atreya/dao/AppointmentDao.java
@@ -1,5 +1,14 @@
 package com.mphasis.atreya.dao;
 
-public interface AppointmentDao {
+import java.util.List;
 
+import com.mphasis.atreya.entities.Appointment;
+
+public interface AppointmentDao {
+	public void requestAppointment(Appointment a);
+	public void deleteAppointment(String id);
+	public void updateAppointment(Appointment appointment);
+	public Appointment getById(String id);
+	public List<Appointment> getAll();
+	public List<Appointment> getAppointmentByDoctid(String doctid);
 }
