4450401daa166b496f68f68c238bdf948a7fd30f
hemalatha995 <49521856+hemalatha995@users.noreply.github.com>
2019-05-09 09:47:00 +0530

    Update ReporterDao.java
    package imported

--- Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
+++ Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
@@ -1,5 +1,5 @@
 package com.mphasis.atreya.dao;
-
+import com.mphasis.atreya.entities.Reporter;
 public interface ReporterDao {
 public Reporter login(String repid,String pwd);
 	public void insertReporter(Reporter reporter);
