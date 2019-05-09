21733a89d5bd54c4a712397ef171cee67cf7cf81
hemalatha.k <hemalatha.k@WKSBAN24TR17020.corp.mphasis.com>
2019-05-09 09:55:11 +0530

    added import

--- Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
+++ Atreya/src/main/java/com/mphasis/atreya/dao/ReporterDao.java
@@ -1,9 +1,7 @@
 package com.mphasis.atreya.dao;
-
 import com.mphasis.atreya.entities.Reporter;
-
 public interface ReporterDao {
-	public Reporter login(String repid,String pwd);
+public Reporter login(String repid,String pwd);
 	public void insertReporter(Reporter reporter);
     public void deleteReporter(String repid);
     public void updateReporter(Reporter reporter);
