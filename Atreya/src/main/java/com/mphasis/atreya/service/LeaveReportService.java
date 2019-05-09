66509ac99d03e0b041d6b585a2ae5331ed9aebe4
hemalatha995 <49521856+hemalatha995@users.noreply.github.com>
2019-05-09 14:58:56 +0530

    Add files via upload
    added services for leavereport

---
+++ Atreya/src/main/java/com/mphasis/atreya/service/LeaveReportService.java
@@ -0,0 +1,8 @@
+package com.mphasis.atreya.service;
+
+import com.mphasis.atreya.entities.LeaveReport;
+
+public interface LeaveReportService {
+	public void applyLeave(LeaveReport leaveReport);
+	public void manageLeave(LeaveReport leaveReport);
+}
