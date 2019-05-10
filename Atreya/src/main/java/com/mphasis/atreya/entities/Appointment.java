570d30944db18edc616089ed6d258844a72bcb8f
mahesh.bv <mahesh.bv@WKSBAN24TR17012.corp.mphasis.com>
2019-05-10 11:00:36 +0530

    sequence generator is added to appointment

--- Atreya/src/main/java/com/mphasis/atreya/entities/Appointment.java
+++ Atreya/src/main/java/com/mphasis/atreya/entities/Appointment.java
@@ -1,14 +1,28 @@
 package com.mphasis.atreya.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import org.hibernate.annotations.*;
+
+import com.mphasis.atreya.util.StringPrefixedSequenceIdGenerator;
 
 @Entity
 public class Appointment {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "appointment_seq")
+    @GenericGenerator(
+        name = "appointment_seq", 
+        strategy = "com.mphasis.atreya.util.StringPrefixedSequenceIdGenerator", 
+        parameters = {
+            @Parameter(name = StringPrefixedSequenceIdGenerator.INCREMENT_PARAM, value = "2"),
+            @Parameter(name = StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value = "APP"),
+            @Parameter(name = StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value = "%03d") })
+
 	private String appid;
 	private String appdetails;
 	@Override
