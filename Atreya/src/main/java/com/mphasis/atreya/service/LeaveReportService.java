d23a407c853377a15fd9f17294bc0815af04bb95
hemalatha995 <49521856+hemalatha995@users.noreply.github.com>
2019-05-15 15:11:23 +0530

    Update LeaveReportService.java
    added getAllReports service

--- Atreya/src/main/java/com/mphasis/atreya/service/LeaveReportService.java
+++ Atreya/src/main/java/com/mphasis/atreya/service/LeaveReportService.java
@@ -6,4 +6,5 @@ import com.mphasis.atreya.exceptions.ClinicExceptions;
 public interface LeaveReportService {
 	public void applyLeave(LeaveReport leaveReport) throws ClinicExceptions;
 	public void manageLeave(LeaveReport leaveReport) throws ClinicExceptions;
+	public List<LeaveReport> getAllReports() throws ClinicExceptions;
 }
